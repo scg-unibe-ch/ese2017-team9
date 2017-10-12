3c58f63d25d9d15509287332f4610614aa0b1736
Manuela Eschler <manu.eschler@gmail.com>
2017-10-12 10:44:41 +0200

    Project from the scratch
    Started project from the scratch with better folder structure.
    Functions:
    - Login/Logout
    - div. sites
    - Create/update User table

---
+++ src/main/java/hello/MvcConfig.java
@@ -0,0 +1,28 @@
+package hello;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/").setCachePeriod(3600).resourceChain(true).addResolver(new PathResourceResolver());
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:css/");
+    }
+
+}
