9a3b7b20e8efd5e88d9e524e4fdb5cc1f0f27c08
Manuela Eschler <manu.eschler@gmail.com>
2017-10-17 11:22:36 +0200

    Access to current user
    Adjustments in user and myProfile display

---
+++ src/main/java/hello/CurrentUserController.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class CurrentUserController {
+
+    @ModelAttribute("currentUser")
+    public User getCurrentUser(@AuthenticationPrincipal User currentUser) {
+        return (currentUser == null) ? null: currentUser;
+    }
+}
